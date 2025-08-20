941a507e8fac143dc49bbb459b58de65d9492676
AYOPELUMI <39781596+AYOPELUMI@users.noreply.github.com>
2025-08-20 13:09:31 +0100

    Delete Documents directory

--- Documents/Flutter/ride_hailing_app/android/app/src/main/kotlin/com/example/ride_hailing_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.ride_hailing_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- Documents/Flutter/ride_hailing_app/lib/res/utils.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-void dismissKeyboard() {
-  FocusManager.instance.primaryFocus?.unfocus();
-}
